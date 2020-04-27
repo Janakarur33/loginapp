ff100b98df767df78d0e97b1003fe085f3d8060c
Janarthanan <jana.singaravel@gmail.com>
2020-04-27 13:53:26 +0530

    added sysout

--- src/main/java/in/co/jana/LoginappApplication.java
+++ src/main/java/in/co/jana/LoginappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoginappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("login app is created");
 		SpringApplication.run(LoginappApplication.class, args);
 	}
 
